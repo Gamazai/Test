67d4e81ebfb02eaa007b9d4eb984c5417e54fbb9
Gamazai <43391757+Gamazai@users.noreply.github.com>
2020-05-19 19:15:24 +0300

    Подцеплен отдельный файл

--- JS_OO/script.js
+++ JS_OO/script.js
@@ -1,5 +1,8 @@
-<script type="text/javascript">
-  document.write("Console text");
-console.log("Information for OIO");
-</script>
 
+'use strict'
+
+async function main () {
+  console.log('Information for OIO')
+}
+
+main().catch(e => console.error('Error:', e))
